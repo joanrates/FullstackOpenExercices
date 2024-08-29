f71e1c7f6489f9fe68345bdc33fc06e12b10c83e
Joan Ratés <joanratesmas@gmail.com>
2024-08-29 17:41:08 +0200

    Part 1 Exercice 6 unicafe finished

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,25 +5,54 @@ const Title = ({text}) => (
 const Button = ({clickHandler, text}) =>(
   <button onClick={clickHandler}>{text}</button>
 )
-const Display = ({text, state}) => (
-  <div>
-    {text} {state}
-  </div>
+const StatisticLine = ({text, value}) =>(
+  <tr>
+    <td>{text}</td> 
+    <td>{value}</td>
+    </tr>
 )
+const Statistics = ({good, neutral, bad, avg, posperc}) => {
+  if (good+neutral+bad === 0){
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  } else 
+    return(
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good}/>
+          <StatisticLine text="neutral" value={neutral}/>
+          <StatisticLine text="bad" value={bad}/>
+          <StatisticLine text="average" value={avg}/>
+          <StatisticLine text="positive" value={posperc+" %"}/>
+        </tbody>
+      </table> 
+    )
+}
+
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  const [avg, setAvg] = useState(0)
+  const [posperc, setPosperc] = useState(0)
   const handlerGen = (state, setState, text)=>{
     return ()=>{ 
       const newState = state +1
       setState(newState)
-      console.log(text, "will be", newState)
+      refreshAvg(text)
     }
   }
-
+  const refreshAvg = (text) => {
+    let modifyer = 0
+    if(text === "good") modifyer = 1
+    setPosperc((good+modifyer)/(good+neutral+bad+1)*100)
+    if ( text === "bad") modifyer = -1
+    setAvg((good-bad+modifyer)/(good+neutral+bad+1))
+  }
   return (
     <div>
       <Title text={"Give us feedback"}/>
@@ -31,9 +60,7 @@ const App = () => {
       <Button clickHandler={handlerGen(neutral,setNeutral,"neutral")} text={"neutral"}></Button>
       <Button clickHandler={handlerGen(bad,setBad,"bad")} text={"bad"}></Button>
       <Title text={"statistics"}/>
-      <Display text="good" state={good}/>
-      <Display text="neutral" state={neutral}/>
-      <Display text="bad" state={bad}/>
+      <Statistics good={good} neutral={neutral} bad={bad} avg={avg} posperc={posperc}/>
     </div>
   )
 }
